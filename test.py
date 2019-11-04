858af9ddf4916ecd2c679b5ce1c3e1ea37fec83e
Zhengtao Zhang <57373571+ZHZ068@users.noreply.github.com>
2019-11-04 13:46:27 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+
+
+def mt(a, b, c, d):
+	def func(x):
+		if x == 'AA':
+			return a
+		elif x == 'BB':
+			return b
+		elif x == 'CC':
+			return c
+		elif x == 'DD':
+			return d
+	return func
+
+
